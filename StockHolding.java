d81fde2f91fb28cf45cb9959dad0d445ba7da5c2
JardinaGomez <ja19gome@siena.edu>
2023-02-10 16:22:17 -0500

    Started wprking on StockHolding class, created instance variables

--- StockHolding.java
+++ StockHolding.java
@@ -1,16 +1,23 @@
 /**
  * Write a description of class Asset here.
  *
- * @author (your name)
+ * @author (Jardina Gomez and Maggie F)
  * @version (a version number or a date)
  */
 public class StockHolding
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
-        // symbol, name, numShares, price, numShares * price); 
-    // }
+  
+    private String stockSymbol;
+    private String stockName;
+    private String numSharesHeld;
+    private String currentStockPricePerShare;
+    
+    
+ @Override
+    public String toString()
+     {
+         //DO NOT EDIT THIS METHOD.
+         return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
+         symbol, name, numShares, price, numShares * price); 
+     }
 }
